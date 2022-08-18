e84a8cd971876c718017c937cd4c998497941279
limonheiro <nicolastimoteu@gmail.com>
2022-08-17 23:57:56 -0400

    🧳

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM ubuntu:22.04 as base
 ARG DEBIAN_FRONTEND=noninteractive
 
 RUN apt-get update &&\
-    apt-get install -y tesseract-ocr &&\
+    #tesseract-ocr-por: portuguese
+    apt-get install -y tesseract-ocr-por &&\
     apt-get install -y git ffmpeg libsm6 libxext6 python3 python3-pip python3-distutils &&\
     rm -rf /var/lib/apt/lists/* 
 
--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+import base64
 from fastapi import FastAPI, File, UploadFile
 import uvicorn
+
+from fastapi.responses import StreamingResponse
+from io import BytesIO
+
+import pytesseract as ocr
+import numpy as np
+import cv2
+from PIL import Image
 
 app = FastAPI()
 
@@ -8,8 +17,36 @@ def home():
     return "Hello"
 
 @app.post("/files/")
-async def create_file(file: bytes = File()):
-    return {"file_size": len(file)}
+async def create_file(file: UploadFile = File(...)):  
+
+    contents = file.file.read()
+    contents = np.fromstring(contents, np.uint8)
+
+    # https://nanonets.com/blog/ocr-with-tesseract/
+
+    # load the example image and convert it to grayscale
+    image = cv2.imdecode(contents, cv2.IMREAD_COLOR)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # apply thresholding to preprocess the image
+    _, gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    #apply median blurring to remove any blurring
+    gray = cv2.medianBlur(gray, 3)
+    # cv2.imwrite(file.filename, gray)
+    phrase = ocr.image_to_string(gray, lang='por')
+
+    format = "."+file.filename.split(".")[-1]
+
+    img = cv2.resize(image, (64,64))
+    _,img_res = cv2.imencode(format, img)
+    encoded_image_string = base64.urlsafe_b64encode(img)
+
+    return {
+        "prase": phrase, 
+        "img_input": encoded_image_string
+        }
+    # return {"file_size": len(file)}
 
 
 @app.post("/uploadfile/")
